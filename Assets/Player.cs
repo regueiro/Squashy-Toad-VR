ad6cc38521b0e10b000310af740fc51c2f181e17
Santiago Regueiro <santi@regueiro.es>
2016-01-07 03:07:11 +0100

    Lecture 17

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,17 +4,13 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
-    //public Text gazeText;
-
     public float jumpAngleInDegrees;
     public int jumpSpeed;
 
     private CardboardHead head;
     private Rigidbody rb;
     private bool onFloor;
-
-
-    private bool showText = true;
+    private float lastJumpRequestTime = 0f;
 
     // Use this for initialization
     void Start()
@@ -22,39 +18,40 @@ public class Player : MonoBehaviour
         Cardboard.SDK.OnTrigger += PullTrigger;
         head = FindObjectOfType<CardboardHead>();
         rb = GetComponent<Rigidbody>();
-
-
     }
 
     private void PullTrigger()
     {
-        if (onFloor)
+       RequestJump();
+    }
+
+    private void RequestJump()
+    {
+        lastJumpRequestTime = Time.time;
+        rb.WakeUp();
+    }
+
+    private void Jump()
+    {
+        var jumpAngleInRadians = Mathf.Deg2Rad * jumpAngleInDegrees;
+        var projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
+
+        var jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
+        rb.velocity = jumpVector * jumpSpeed;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        var delta = Time.time - lastJumpRequestTime;
+        if (delta < 0.1)
         {
-            var jumpAngleInRadians = Mathf.Deg2Rad * jumpAngleInDegrees;
-            var projectedVector = Vector3.ProjectOnPlane(head.Gaze.direction, Vector3.up);
-
-            var jumpVector = Vector3.RotateTowards(projectedVector, Vector3.up, jumpAngleInRadians, 0);
-            rb.velocity = jumpVector * jumpSpeed;
+            Jump();
+            lastJumpRequestTime = 0f;
         }
-
-        //rb.AddForce(head.Gaze.direction * 1000);
-        //showText = !showText;
     }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        onFloor = true;
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        onFloor = false;
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        //gazeText.enabled = showText;
-        //gazeText.text = head.Gaze.ToString();
     }
 }
