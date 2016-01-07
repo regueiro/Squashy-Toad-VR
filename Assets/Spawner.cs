bf8fba97972c92ed6d6ba61615d715565da7986b
Santiago Regueiro <santi@regueiro.es>
2016-01-07 02:53:55 +0100

    Lecture 16

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -5,16 +5,34 @@ public class Spawner : MonoBehaviour
 {
 
     public GameObject[] lanePrefabs;
+    public float spawnHorizon = 500f;
+    public float laneWidth = 2;
+    public GameObject player;
+    public Transform spawnerParent;
+
+
+    private float nextLaneOffset = 0;
 
     // Use this for initialization
     void Start ()
     {
-        var lane = lanePrefabs[0];
-        Instantiate(lane, Vector3.zero, Quaternion.identity);
     }
     
     // Update is called once per frame
-    void Update () {
-    
+    void Update ()
+    {
+        var forwardPosition = player.transform.position.z; 
+        while (forwardPosition + spawnHorizon > nextLaneOffset)
+        {
+            var randomLaneIndex = Random.Range(0, lanePrefabs.Length);
+            var lane = lanePrefabs[randomLaneIndex];
+            var nextLanePosition = nextLaneOffset*Vector3.forward;
+
+            var newLaneObject = Instantiate(lane, nextLanePosition, Quaternion.identity) as GameObject;
+            newLaneObject.transform.parent = spawnerParent;
+            nextLaneOffset += laneWidth;
+        }
+        
+
     }
 }
