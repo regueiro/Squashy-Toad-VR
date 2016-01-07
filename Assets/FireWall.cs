c5a492f6cd70e0b3fff95d3d7ab3febc4763c99c
Santiago Regueiro <santi@regueiro.es>
2016-01-07 03:27:13 +0100

    Lecture 18

---
+++ Assets/FireWall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var player = GameObject.Find("Player");
+	    var delta = player.transform.position - transform.position;
+	    var projectedDelta = Vector3.Project(delta, Vector3.left);
+
+	    transform.position += projectedDelta;
+	}
+}
